a79165adb7ba46b8f74f36e777979aacb2087952
Angel <torres.alvarez.a18@gmail.com>
2025-11-21 08:24:26 -0600

    Uploading an existing project

---
+++ proyecto_titulacion/lib/common/navigation/router/router.dart
@@ -0,0 +1,17 @@
+import 'package:proyecto_titulacion/common/navigation/router/routes.dart';
+import 'package:proyecto_titulacion/features/trip/ui/trips_list/trips_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart'; 
+
+final router = GoRouter( 
+  routes: [ 
+    GoRoute( 
+      path: '/', 
+      name: AppRoute.home.name, 
+      builder: (context, state) => const TripsListPage(), 
+    ), 
+  ], 
+  errorBuilder: (context, state) => Scaffold( 
+    body: Center( child: Text(state.error.toString()), ), 
+  ),
+);
